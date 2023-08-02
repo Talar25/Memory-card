295b1a3249a9e882bb3cb70775e58699f8a6a5cf
Talar25 <90799331+Talar25@users.noreply.github.com>
2023-08-02 11:49:55 +0200

    Fix loading imgs

--- src/components/Main-page/Main-page.jsx
+++ src/components/Main-page/Main-page.jsx
@@ -58,10 +58,17 @@ export function MainPage({ addScore, restartScore, checkWin, characters }) {
 
 // eslint-disable-next-line react/prop-types
 function Card({ children, url, handleClick, id }) {
+  const [didLoad, setLoad] = useState(false);
   // const url = data.forms.url;
+  function handleLoading() {
+    setLoad(true);
+  }
+
+  const style = didLoad ? {} : { visibility: "hidden" };
+
   return (
-    <div className="main__card" onClick={() => handleClick(id)}>
-      <img src={url} />
+    <div style={style} className="main__card" onClick={() => handleClick(id)}>
+      <img src={url} onLoad={handleLoading} />
       <p className="main__card-name">{children}</p>
     </div>
   );
